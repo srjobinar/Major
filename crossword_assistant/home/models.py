4d045996d656679a6a0c0272277d3aa4e65767d8
ashwin sekhar <sekhar.ashwin@gmail.com>
2017-04-25 21:09:56 +0530

    testing

--- crossword_assistant/home/models.py
+++ crossword_assistant/home/models.py
@@ -8,4 +8,4 @@ class Clue(models.Model):
     clue = models.CharField(max_length=250)
     clue_number = models.PositiveIntegerField()
     answer_length = models.PositiveIntegerField()
-    across_down = models.PositiveIntegerField()gid
+    across_down = models.PositiveIntegerField()
